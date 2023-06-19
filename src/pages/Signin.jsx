f793cd8b8d73c5199627072339059cbb649277fb
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-19 23:11:14 +0900

    feat: markup signin page

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Signin = () => {
   console.log();
-  return <>Signin</>;
+  return (
+    <>
+      <form>
+        <label>
+          이메일
+          <input data-testid="email-input" type="text" />
+        </label>
+        <label>
+          비밀번호
+          <input data-testid="password-input" type="password" />
+        </label>
+        <button data-testid="signin-button">로그인</button>
+      </form>
+    </>
+  );
 };
 
 export default Signin;
