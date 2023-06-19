12b8f8be18123bb16b39e78c7921071bfe43a985
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-19 17:47:37 +0900

    feat: implement routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
-}
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Root, Signin, Signup, Todo } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/signin',
+        element: <Signin />,
+      },
+      {
+        path: '/signup',
+        element: <Signup />,
+      },
+      {
+        path: '/todo',
+        element: <Todo />,
+      },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
