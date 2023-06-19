2c21ccadb3a020c783083677220c874b7a7752bf
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-19 23:11:01 +0900

    Feat: add global style

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    margin: 0 auto;
+  }
+`;
+
+export default GlobalStyle;
