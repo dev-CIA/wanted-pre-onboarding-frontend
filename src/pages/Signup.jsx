c016b2b629b9c1e0e5967fe9dd82ced05e21a4e5
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-20 10:15:29 +0900

    feat: implement early version of signup UI

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Signup = () => {
   console.log();
-  return <>Signup</>;
+  return (
+    <Container>
+      <Title>회원가입</Title>
+      <Form>
+        <Label>
+          이메일
+          <Input data-testid="email-input" type="text" />
+        </Label>
+        <Label>
+          비밀번호
+          <Input data-testid="password-input" type="password" />
+        </Label>
+        <Button data-testid="signin-button">회원가입</Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Signup;
+
+const Container = styled.div`
+  margin: 2em auto;
+`;
+
+const Title = styled.div`
+  margin: 0.5em;
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  width: 20em;
+  border: 1px solid black;
+  padding: 2em;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 2em;
+`;
+
+const Button = styled.button`
+  height: 2.5em;
+  font-weight: 500;
+`;
