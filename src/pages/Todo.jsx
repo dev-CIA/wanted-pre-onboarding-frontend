e77ed6d89e2b7b23f174060a54a0aa6e8634dcaa
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-20 21:04:33 +0900

    feat: markup initial version of Todo page

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
 
+const mockData = [
+  {
+    id: 1,
+    todo: 'todo2',
+    isCompleted: false,
+    userId: 1,
+  },
+  {
+    id: 2,
+    todo: 'todo3',
+    isCompleted: true,
+    userId: 1,
+  },
+];
+
 const Todo = () => {
   console.log();
-  return <>Todo</>;
+  return (
+    <>
+      <div>
+        <input data-testid="new-todo-input" />
+        <button data-testid="new-todo-add-button">추가</button>
+      </div>
+      {mockData.map(data => (
+        <li key={data.id}>
+          <label>
+            <input type="checkbox" checked={data.isCompleted} onChange={() => {}} />
+            <span>{data.todo}</span>
+          </label>
+          <button data-testid="modify-button">수정</button>
+          <button data-testid="delete-button">삭제</button>
+        </li>
+      ))}
+    </>
+  );
 };
 
 export default Todo;
