02e26581e0f89a9c45f4ee4c2520516c6b7dcf99
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-21 12:27:14 +0900

    feat: add authentication to todos api

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 import URL from '../constants/todoUrl';
 
+const accessToken = localStorage.getItem('accessToken') ?? '';
+
 const getTodos = async () => {
-  const { data } = await axios.get(`${URL}/todos`);
+  const { data } = await axios.get(`${URL}/todos`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
   return data;
 };
 
