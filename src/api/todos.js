0a4177bde34df3af521eb160c9a4def1a95f1f1c
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-21 20:50:15 +0900

    feat: change axios config to a variable

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 import URL from '../constants/todoUrl';
 
-const accessToken = localStorage.getItem('accessToken') ?? '';
+const accessToken = localStorage.getItem('accessToken');
+
+const todosConfig = {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+};
 
 const getTodos = async () => {
-  const { data } = await axios.get(`${URL}/todos`, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-
-  return data;
+  try {
+    const response = await axios.get(`${URL}/todos`, todosConfig);
+    return response;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 export default getTodos;
