ab1f195c14c8854601e7a846980b4325676a8af7
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-21 11:04:04 +0900

    feat: add file path to index.js

---
+++ src/api/index.js
@@ -0,0 +1,1 @@
+export { postSignin, postSignUp } from './auth';
--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as AuthFormContainer } from './AuthFormContainer';
 export { default as FormLabel } from './FormLabel';
 export { default as FormInput } from './FormInput';
 export { default as SubmitButton } from './SubmitButton';
+export { default as AuthMessage } from './AuthMessage';
