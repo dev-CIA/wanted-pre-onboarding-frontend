5e8a6cce1d2a60358bda15971dd3e494391de2b6
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-06-21 22:28:48 +0900

    feat: implement to update todo

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { CheckCompleted } from '.';
-import { deleteTodo } from '../../api';
+import { updateTodo, deleteTodo } from '../../api';
 
 const TodoItem = ({ data, userData, setUserData }) => {
   const [isModifyMode, setIsModifiyMode] = React.useState(false);
+  const [updatedTodo, setUpdatedTodo] = React.useState('');
+
+  const updateData = async ({ id, isCompleted }) => {
+    await updateTodo({ id, todo: updatedTodo, isCompleted });
+    setUserData(userData.map(data => (data.id === id ? { ...data, todo: updatedTodo } : data)));
+    setIsModifiyMode(false);
+  };
 
   const deleteData = async id => {
     await deleteTodo(id);
@@ -15,18 +22,47 @@ const TodoItem = ({ data, userData, setUserData }) => {
       <li>
         <label>
           <CheckCompleted {...data} />
-          {isModifyMode ? <input /> : <span>{data.todo}</span>}
+          {isModifyMode ? (
+            <input
+              data-testid="modify-input"
+              ref={node => node?.focus()}
+              value={updatedTodo}
+              onChange={e => {
+                setUpdatedTodo(e.target.value);
+              }}
+            />
+          ) : (
+            <span>{data.todo}</span>
+          )}
         </label>
-        <button
-          data-testid="modify-button"
-          onClick={() => {
-            setIsModifiyMode(true);
-          }}>
-          수정
-        </button>
-        <button data-testid="delete-button" onClick={() => deleteData(data.id)}>
-          삭제
-        </button>
+        {isModifyMode ? (
+          <>
+            <button data-testid="submit-button" onClick={() => updateData(data)}>
+              제출
+            </button>
+            <button
+              data-testid="cancel-button"
+              onClick={() => {
+                setIsModifiyMode(false);
+              }}>
+              취소
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              data-testid="modify-button"
+              onClick={() => {
+                setIsModifiyMode(true);
+                setUpdatedTodo(data.todo);
+              }}>
+              수정
+            </button>
+            <button data-testid="delete-button" onClick={() => deleteData(data.id)}>
+              삭제
+            </button>
+          </>
+        )}
       </li>
     </>
   );
